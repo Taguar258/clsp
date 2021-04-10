7185ffcc4f5a2d8ecab0eeca393a4968fcf46008
Taguar258 <taguar466@gmail.com>
2021-04-10 16:40:14 +0200

    Unit Tests

---
+++ tests/tests.py
@@ -0,0 +1,45 @@
+import pytest
+from clsp import SelectionPrompt, select
+
+
+@pytest.fixture
+def sp():
+
+    return SelectionPrompt([1, 2, 3, 23, 11, 5], rows=1)
+
+
+def basic_tests(sp):
+
+    sp._navigate_menu(1)
+    sp._refresh_currently_shown()
+    sp._return_selection()
+    selected = sp._return_placeholder
+
+    assert selected.value == "2"
+    assert selected.index == 1
+    assert sp._current == 1
+    assert sp._current_position == [1, 1]
+
+
+def test_search(sp):
+
+    print("Please search and select number '23'.")
+
+    sp._search = "23"
+    sp._refresh_currently_shown()
+    sp._return_selection()
+    selected = sp._return_placeholder
+
+    assert selected.value == "23"
+    assert selected.index == 3
+    assert sp._current == 0
+    assert sp._current_position == [0, 1]
+
+
+if __name__ == "__main__":
+
+    inp = [1, 2, 3, 23, 11, 5]
+
+    out = select(inp)
+
+    print(out, out.index)

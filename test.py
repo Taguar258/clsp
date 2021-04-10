363eab95eb2570623204eff6fd1e39bf56a2a688
Taguar258 <36562445+Taguar258@users.noreply.github.com>
2021-04-10 23:34:23 +0200

    Removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pycountry
-from clsp import select
-
-inp = [c.alpha_2 for c in pycountry.countries]
-
-out = select(inp, info="Select your Country:", rows=10)
-
-print("You selected: " + out.value)
